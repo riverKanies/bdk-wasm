8408e189ece532afdfaf77f84acb25a00ebf3897
Dario Anongba Varela <dario.anongba@gmail.com>
2024-11-05 19:26:40 +0100

    working skeleton code

--- README.md
+++ README.md
@@ -65,4 +65,3 @@ wasm-pack publish
   between WebAssembly and JavaScript.
 * [`console_error_panic_hook`](https://github.com/rustwasm/console_error_panic_hook)
   for logging panic messages to the developer console.
-* `LICENSE-APACHE` and `LICENSE-MIT`: most Rust projects are licensed this way, so these are included for you
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 mod utils;
 
+use std::{cell::RefCell, collections::BTreeSet, io::Write, rc::Rc};
+
+use bdk_esplora::{
+    esplora_client::{self, AsyncClient},
+    EsploraAsyncExt,
+};
+use bdk_wallet::{bitcoin::Network, ChangeSet, KeychainKind, Wallet};
+use js_sys::Promise;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::future_to_promise;
+use web_sys::console;
+
+const PARALLEL_REQUESTS: usize = 5;
 
 #[wasm_bindgen]
 extern "C" {}
@@ -8,4 +20,107 @@ extern "C" {}
 #[wasm_bindgen]
 pub fn greet() -> String {
     "Hello, bdk-wasm!".into()
+}
+
+#[wasm_bindgen]
+pub struct WalletWrapper {
+    wallet: Rc<RefCell<Wallet>>,
+    client: Rc<RefCell<AsyncClient>>,
+}
+
+#[wasm_bindgen]
+impl WalletWrapper {
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        network: String,
+        external_descriptor: String,
+        internal_descriptor: String,
+        esplora_url: String,
+    ) -> Result<WalletWrapper, String> {
+        let network = match network.as_str() {
+            "mainnet" => Network::Bitcoin,
+            "testnet" => Network::Testnet,
+            "testnet4" => Network::Testnet4,
+            "signet" => Network::Signet,
+            "regtest" => Network::Regtest,
+            _ => return Err("Invalid network".into()),
+        };
+
+        let wallet_opt = Wallet::load()
+            .descriptor(KeychainKind::External, Some(external_descriptor.clone()))
+            .descriptor(KeychainKind::Internal, Some(internal_descriptor.clone()))
+            .extract_keys()
+            .check_network(network)
+            .load_wallet_no_persist(ChangeSet::default())
+            .map_err(|e| format!("{:?}", e))?;
+
+        let wallet = match wallet_opt {
+            Some(wallet) => wallet,
+            None => Wallet::create(external_descriptor, internal_descriptor)
+                .network(network)
+                .create_wallet_no_persist()
+                .map_err(|e| format!("{:?}", e))?,
+        };
+
+        let client = esplora_client::Builder::new(&esplora_url)
+            .build_async()
+            .map_err(|e| format!("{:?}", e))?;
+
+        Ok(WalletWrapper {
+            wallet: Rc::new(RefCell::new(wallet)),
+            client: Rc::new(RefCell::new(client)),
+        })
+    }
+
+    pub fn sync(&self, stop_gap: usize) -> Promise {
+        let wallet = Rc::clone(&self.wallet);
+        let client = Rc::clone(&self.client);
+
+        future_to_promise(async move {
+            console::log_1(&"before sync".into());
+
+            let request = wallet.borrow().start_full_scan().inspect({
+                let mut stdout = std::io::stdout();
+                let mut once = BTreeSet::<KeychainKind>::new();
+                move |keychain, spk_i, _| {
+                    if once.insert(keychain) {
+                        console::log_1(&format!("\nScanning keychain [{:?}]", keychain).into());
+                    }
+                    console::log_1(&format!(" {:<3}", spk_i).into());
+                    stdout.flush().expect("must flush")
+                }
+            });
+
+            let update = client
+                .borrow()
+                .full_scan(request, stop_gap, PARALLEL_REQUESTS)
+                .await
+                .map_err(|e| format!("{:?}", e))?;
+
+            wallet
+                .borrow_mut()
+                .apply_update(update)
+                .map_err(|e| format!("{:?}", e))?;
+
+            console::log_1(&"after sync".into());
+
+            Ok("done".into())
+        })
+    }
+
+    #[wasm_bindgen]
+    pub fn balance(&self) -> u64 {
+        let balance = self.wallet.borrow().balance();
+        balance.total().to_sat()
+    }
+
+    #[wasm_bindgen]
+    pub fn get_new_address(&self) -> String {
+        let address = self
+            .wallet
+            .borrow_mut()
+            .next_unused_address(KeychainKind::External);
+
+        address.to_string()
+    }
 }
