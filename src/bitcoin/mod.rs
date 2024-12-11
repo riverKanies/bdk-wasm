84bef00790865744db5a0197f1ed16898cf7f09d
Dario Anongba Varela <dario.anongba@gmail.com>
2024-12-11 12:27:10 +0100

    fix: ci (#10)

--- src/bitcoin/mod.rs
+++ src/bitcoin/mod.rs
@@ -10,14 +10,8 @@ mod esplora_wallet;
 #[cfg(feature = "snap")]
 mod snap_wallet;
 
-#[cfg(feature = "bitcoind")]
-mod rpc_wallet;
-
 #[cfg(feature = "esplora")]
 pub use esplora_wallet::EsploraWallet;
 
 #[cfg(feature = "snap")]
 pub use snap_wallet::SnapWallet;
-
-#[cfg(feature = "bitcoind")]
-pub use rpc_wallet::RpcWallet;
